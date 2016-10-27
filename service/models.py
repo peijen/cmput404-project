d96df6084e4f13dae3d9a4c336c7336bc5a56441
root <root@MATE.localdomain>
2016-10-26 19:19:32 -0600

    Made the Post and Comment Tables in the DB

--- service/models.py
+++ service/models.py
@@ -13,3 +13,24 @@ class Author(models.Model):
     url = models.CharField(max_length=500)
     github = models.CharField(max_length=500)
     userID = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=150)
+    source = models.CharField(max_length=150)
+    origin = models.CharField(max_length=150)
+    description = models.CharField(max_length=150)
+    contentType = models.CharField(max_length=150)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    catagories = models.TextField(null=True)
+    published = models.DateTimeField(auto_now=True)
+    visibility = models.CharField(max_length=100)
+
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    comment = models.TextField()
+    contentType = models.CharField(max_length=150)
+    published = models.DateTimeField(auto_now=True)
