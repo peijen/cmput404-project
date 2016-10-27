6fc3fcfd569c54062d02c8c236c72bf147ff1dae
root <root@MATE.localdomain>
2016-10-26 18:25:06 -0600

    Push before migration

--- service/models.py
+++ service/models.py
@@ -2,4 +2,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+import uuid
+
+from django.contrib.auth.models import AbstractUser
+
+class Users(AbstractUser):
+    authorID = models.ForeignKey(Authors, on_delete=models.CASCADE)
+
+
+class Author():
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    host = models.CharField()
+    displayName = models.CharField(max_length=50)
+    url = models.CharField()
+    github = models.CharField()
