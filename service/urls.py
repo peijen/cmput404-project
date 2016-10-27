baa9a38d0c6ee71dab47b6cbd9c7a69d6966bc1a
tbrockman <tbrockma@ualberta.ca>
2016-10-27 00:08:15 -0600

    some tests and endpoints for posts

--- service/urls.py
+++ service/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^posts/$', views.posts_handler),
+    url(r'^posts/$', views.posts_handler_generic),
+    url(r'^posts/(?P<id>\d+)/$', views.posts_handler_specific),
     url(r'^author/$', views.author_handler),
     url(r'^friends/$', views.friend_handler),
 ]
