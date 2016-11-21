92808e922c64499cbbd30b5c64a74542e69134b5
root <root@MATE.localdomain>
2016-11-21 15:34:52 -0700

    Quick Fix

--- service/admin.py
+++ service/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Author, Comment, Post, FriendRequest
+from .models import Author, Comment, Post, FriendRequest, Nodes
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
