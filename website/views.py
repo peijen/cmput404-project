57f3f7496ba119a503e3d39a59632a6898d07802
tbrockman <tbrockma@ualberta.ca>
2016-11-29 14:54:29 -0700

    return 404 if profile not found off server

--- website/views.py
+++ website/views.py
@@ -18,63 +18,63 @@ from requests.auth import HTTPBasicAuth
 
 @login_required(login_url="login/")
 def home(request):
-    return render(request, "home.html")
+	return render(request, "home.html")
 
 
 def register_success(request):
-    return render(request, "register_success.html")
+	return render(request, "register_success.html")
 
 
 class UserRegisterForm(View):
-    form_class = UserRegisterForm
-    template_name = 'register.html'
+	form_class = UserRegisterForm
+	template_name = 'register.html'
 
-    def get(self, request):
-        form = self.form_class(None)
-        return render(request, self.template_name, {'form': form})
+	def get(self, request):
+		form = self.form_class(None)
+		return render(request, self.template_name, {'form': form})
 
-    def post(self, request):
-        form = self.form_class(request.POST)
+	def post(self, request):
+		form = self.form_class(request.POST)
 
-        if form.is_valid():
-            user = form.save(commit=False)
+		if form.is_valid():
+			user = form.save(commit=False)
 
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user.set_password(password)
-            user.is_active = False
-            user.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+			user.set_password(password)
+			user.is_active = False
+			user.save()
 
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return render(request, "home.html")
-            return render(request, "register_success.html")
-        return render(request, self.template_name, {'form': form})
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				if user.is_active:
+					login(request, user)
+					return render(request, "home.html")
+			return render(request, "register_success.html")
+		return render(request, self.template_name, {'form': form})
 
 
 @login_required(login_url="login/")
 def make_post(request):
-    return render(request, "make_post.html")
+	return render(request, "make_post.html")
 
 
 @login_required(login_url="login/")
 @transaction.atomic
 def update_profile(request):
-    if request.method == 'POST':
-        profile_form = ProfileForm(request.POST, instance=request.user.author)
-        if profile_form.is_valid():
-            profile_form.save()
-            # TODO: send some verification message
+	if request.method == 'POST':
+		profile_form = ProfileForm(request.POST, instance=request.user.author)
+		if profile_form.is_valid():
+			profile_form.save()
+			# TODO: send some verification message
 
-            # TODO: should have an else: send some failure message. possibly
-            # not needed.
-    else:
-        profile_form = ProfileForm(instance=request.user.author)
-    return render(request, 'profile.html', {
-        'profile_form': profile_form
-    })
+			# TODO: should have an else: send some failure message. possibly
+			# not needed.
+	else:
+		profile_form = ProfileForm(instance=request.user.author)
+	return render(request, 'profile.html', {
+		'profile_form': profile_form
+	})
 
 
 @login_required(login_url="login/")
@@ -83,20 +83,23 @@ def view_profile(request, id):
 		host = request.GET.get('host', '')
 		if (host != 'https://cmput404t02.herokuapp.com/service/'):
 			nodes = Nodes.objects.all()
-
+			json_profile = {}
 			for node in nodes:
 				if (node.url == host):
 					if (node.useauth):
 						response = requests.get(node.url + "author/" + str(id), auth=HTTPBasicAuth(node.username, node.password))
 					else:
 						response = requests.get(node.url + "author/", auth=HTTPBasicAuth(node.username, node.password))
-
+						
 					json_profile = json.loads(response.content)
 					break
 			user = request.user
 			request_id = user.author.id
-			json_profile['url'] = json_profile['host'] + 'author/' + str(json_profile['id'])
-			return render(request, 'author.html', {'author': json_profile, 'user_id': request_id, 'request_user': user, 'profile_user': json_profile})
+			if not json_profile:
+				return HttpResponse(status=404)
+			else:
+				json_profile['url'] = json_profile['host'] + 'author/' + str(json_profile['id'])
+				return render(request, 'author.html', {'author': json_profile, 'user_id': request_id, 'request_user': user, 'profile_user': json_profile})
 		else:
 			author = Author.objects.get(id=id)
 			display = Author.objects.get(id=id)
@@ -110,13 +113,13 @@ def view_profile(request, id):
 
 @login_required(login_url="login/")
 def friend_requests(request):
-    if (request.method == 'GET'):
-        return render(request, 'requests.html')
-    return HttpResponse(status=405)
+	if (request.method == 'GET'):
+		return render(request, 'requests.html')
+	return HttpResponse(status=405)
 
 
 @login_required(login_url="login/")
 def friends(request):
-    if (request.method == 'GET'):
-        return render(request, 'friends.html')
-    return HttpResponse(status=405)
+	if (request.method == 'GET'):
+		return render(request, 'friends.html')
+	return HttpResponse(status=405)
