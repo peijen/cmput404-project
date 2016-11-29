953cd5ca33c26628bde05f3ddc8765e6540a80c6
tbrockman <tbrockma@ualberta.ca>
2016-11-29 11:56:44 -0700

    friends

--- cmput404/static/js/requests/requests.controller.js
+++ cmput404/static/js/requests/requests.controller.js
@@ -5,8 +5,17 @@ app.controller("requestCtrl", ['$scope', 'Author', function($scope, Author) {
 
     Author.getFriendRequests().then(function(res) {
         $scope.friend_requests = res.data;
-    }, function(res) {
-
     });
+
+    $scope.acceptRequest = function(request) {
+        Author.addFriend(request.author);
+        for (var i = 0; i < $scope.friend_requests.length; i++) {
+            if ($scope.friend_requests[i].id == request.id) {
+                $scope.friend_requests[i].splice(i, 1);
+                break;
+            }
+        }
+
+    }
 
 }]);
